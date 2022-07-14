f6267b10e6bd301ae97fd50bb578be846ddc54f5
Silvia-Du <silviadu@hotmail.it>
2022-07-14 14:39:17 +0200

    Pulizia base di progetto: parto da zero components

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <MainComp :postList="posts" />
   </div>
 </template>
 
@@ -8,34 +7,23 @@
 
 //import components
 
-import MainComp from './MainComp.vue';
 export default {
     name: 'App',
 
     components: {
-        MainComp
     },
 
     data(){
         return{
-            postApiUrl: '/api/posts',
-            posts: null,
+
         }
     },
 
     methods: {
-        getPosts(){
-            axios.get(this.postApiUrl)
-            .then(response =>{
-                this.posts = response.data.posts;
-                console.log(this.posts);
 
-            })
-        }
     },
 
     mounted(){
-        this.getPosts();
     }
 }
 </script>
